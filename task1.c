24a6a8cd88c28c07a3da8b3e3dbefc67baca81ef
Cole Nola <cnola1050207@gmail.com>
2026-09-24 16:08:39 -0500

    ai initial task 1 code and tested

---
+++ task1.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+#define NUMBER_OF_PLAYS 5
+
+static const int scoring_plays[NUMBER_OF_PLAYS] = {2, 3, 6, 7, 8};
+
+static void find_combinations(int remaining_score, int play_index,
+							  int play_counts[])
+{
+	int count;
+
+	if (play_index == NUMBER_OF_PLAYS - 1) {
+		if (remaining_score % scoring_plays[play_index] == 0) {
+			play_counts[play_index] =
+				remaining_score / scoring_plays[play_index];
+
+			printf("2-point: %d, 3-point: %d, 6-point: %d, "
+				   "7-point: %d, 8-point: %d\n",
+				   play_counts[0], play_counts[1], play_counts[2],
+				   play_counts[3], play_counts[4]);
+		}
+		return;
+	}
+
+	for (count = 0; count * scoring_plays[play_index] <= remaining_score;
+		 count++) {
+		play_counts[play_index] = count;
+		find_combinations(remaining_score -
+							  count * scoring_plays[play_index],
+						  play_index + 1, play_counts);
+	}
+}
+
+int main(void)
+{
+	int score;
+	int play_counts[NUMBER_OF_PLAYS] = {0};
+
+	printf("Enter the score: ");
+	if (scanf("%d", &score) != 1 || score < 0) {
+		printf("Please enter a non-negative whole-number score.\n");
+		return 1;
+	}
+
+	printf("Possible scoring combinations for %d points:\n", score);
+	find_combinations(score, 0, play_counts);
+
+	return 0;
+}
